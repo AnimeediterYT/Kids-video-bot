d3f63be7bf77eb02b58f049033459878cef3abf0
AnimeediterYT <pistha.gd@gmail.com>
2026-06-08 00:32:01 +0530

    Add audio parameter to write_videofile method

--- generate_video.py
+++ generate_video.py
@@ -1,4 +1,11 @@
 from moviepy.editor import *
 
+# background video (black screen)
 clip = ColorClip(size=(1080, 1920), color=(0, 0, 0), duration=6)
-clip.write_videofile("videos/final.mp4", fps=24, codec="libx264")
+
+clip.write_videofile(
+    "videos/final.mp4",
+    fps=24,
+    codec="libx264",
+    audio=False
+)
