de2edae6c008b0b579b3539be9eaecfe234afbb0
AnimeediterYT <pistha.gd@gmail.com>
2026-06-08 00:58:23 +0530

    Adjust video background size and text font size

--- video.py
+++ video.py
@@ -5,9 +5,9 @@ os.makedirs("videos", exist_ok=True)
 
 audio = AudioFileClip("audio/voice.mp3")
 
-bg = ColorClip((1080,1920), color=(0,0,0), duration=audio.duration)
+bg = ColorClip((1920,1080), color=(0,0,0), duration=audio.duration)
 
-txt = TextClip("Moral Story", fontsize=70, color='white')
+txt = TextClip("Moral Story", fontsize=80, color='white')
 txt = txt.set_duration(audio.duration).set_position("center")
 
 video = CompositeVideoClip([bg, txt]).set_audio(audio)
